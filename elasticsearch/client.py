ed524a833e5515158fba8d502cce30646bbf91b8
YushengFeng01 <Yusheng.Feng@clarivate.com>
2019-11-04 21:42:13 +0800

    es client draft

---
+++ elasticsearch/client.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+Elasticsearch Python client:
+https://elasticsearch-py.readthedocs.io/en/master/
+'''
+
+import elasticsearch
+
+def es_draft():
+    # https://elasticsearch-py.readthedocs.io/en/7.0.1/api.html#elasticsearch
+    es = elasticsearch.Elasticsearch([
+        {'host':'localhost', 'port':'9200'}
+    ])
+
+    # https://elasticsearch-py.readthedocs.io/en/7.0.1/api.html#cat
+    response = es.cat.health(v=True, format='json')
+    print(response[0])
+
+if __name__ == '__main__':
+    es_draft()
